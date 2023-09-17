2a771cbe4f237e0e910cfa0f8b61a782dd4fc99e
Toby <tobyyzhang@gmail.com>
2023-09-16 21:38:12 -0400

    update

--- app.js
+++ app.js
@@ -418,7 +418,7 @@ app.post("/stripe", async (req, res) => {
       destination: "stockbet/sdxl-viking",
       input: {
         input_images: `https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/uploads/${emailPrefix}.zip`,
-        num_train_epochs: 6000,
+        num_train_epochs: 8000,
         is_lora: false,
         crop_based_on_salience: false,
         use_face_detection_instead: true,
